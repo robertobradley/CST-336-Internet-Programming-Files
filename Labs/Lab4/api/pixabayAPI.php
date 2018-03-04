d33c69d49c6d1d5c128658fa9b8b66b3d185c9f9
Rbradley <throck58@yahoo.com>
2018-03-04 07:39:08 +0000

    commit for lab 3

--- Labs/Lab4/api/pixabayAPI.php
+++ Labs/Lab4/api/pixabayAPI.php
@@ -4,7 +4,7 @@
 function getImageURLs($keyword, $orientation="horizontal") {
     $curl = curl_init();
     curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=$keyword&image_type=photo&orientation=$orientation&safesearch=true&per_page=100",
+      CURLOPT_URL => "https://pixabay.com/api/?key=8124595-8645c548c4f9d3596e5e0339b&q=$keyword&image_type=photo&orientation=$orientation&safesearch=true&per_page=100",
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
