1f5fbc30567a6e5525e9cdea0a30a921875d990f
Rbradley <throck58@yahoo.com>
2018-04-01 05:24:20 +0000

    pushing for lab 6 again with new db connection

--- dbConnection.php
+++ dbConnection.php
@@ -11,8 +11,8 @@ if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
    $host = $url["host"];
    $dbname = substr($url["path"], 1);
-   $username = $url["user"];
-   $password = $url["pass"];
+   $username = $url["beeae2a6f56b03"];
+   $password = $url["fec867f0"];
 }
 
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
